1efff2a828ec1f856f41b1b211e5d7b3dc3911b6
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-11-03 12:11:31 -0400

    Add LoginForm component

---
+++ src/components/LoginForm.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+// bootstrap components
+import {
+  Form,
+  FormGroup,
+  Col,
+  ControlLabel,
+  FormControl,
+  Checkbox,
+  Button
+} from 'react-bootstrap';
+
+const LoginForm = () => (
+  <Form horizontal>
+    <FormGroup controlId="formHorizontalEmail">
+      <Col componentClass={ControlLabel} sm={2}>
+        Email
+      </Col>
+      <Col sm={10}>
+        <FormControl type="email" placeholder="Email" />
+      </Col>
+    </FormGroup>
+
+    <FormGroup controlId="formHorizontalPassword">
+      <Col componentClass={ControlLabel} sm={2}>
+        Password
+      </Col>
+      <Col sm={10}>
+        <FormControl type="password" placeholder="Password" />
+      </Col>
+    </FormGroup>
+
+    <FormGroup>
+      <Col smOffset={2} sm={10}>
+        <Checkbox>Remember me</Checkbox>
+      </Col>
+    </FormGroup>
+
+    <FormGroup>
+      <Col smOffset={2} sm={10}>
+        <Button type="submit">
+          Sign in
+        </Button>
+      </Col>
+    </FormGroup>
+  </Form>
+)
+
+export default LoginForm;
